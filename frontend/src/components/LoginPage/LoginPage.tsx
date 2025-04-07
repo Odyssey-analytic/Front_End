3ebbcd0e7476c603a49c8ebfb98ce5050112b840
qazalarabali <qazalarabali79@gmail.com>
2025-04-07 11:22:54 +0330

    feat: implement RTL responsive login UI with Google and email auth

---
+++ frontend/src/components/LoginPage/LoginPage.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './LoginPage.css';
+
+const LoginPage = () => {
+  return (
+    <div className="d-flex justify-content-center justify-content-lg-start align-items-center vh-100 login-page-container px-3">
+      <div className="login-box mx-auto ms-lg-5">
+        <h2 className="fw-bold text-start mb-3 login-title">ورود</h2>
+        <p className="text-start login-subtitle">ورود با استفاده از ایمیل</p>
+        <form>
+          <div className="mb-3 position-relative">
+            <input
+              type="email"
+              className="form-control text-start pe-5 login-input text-dark"
+              placeholder=".ایمیل خود را وارد کنید"
+              required
+            />
+            {/* <img
+              src="/src/assets/icons/email_icon.svg"
+              alt="email icon"
+              className="login-input-email-icon"
+            /> */}
+            <button type="submit" className="btn w-100 login-btn">
+              ورود
+            </button>
+
+            <div className="text-muted small px-2 my-3 text-start login-non-register-remember-password">
+              <span className="login-non-register-remember-password">
+                ثبت‌نام نکرده‌اید؟ <a className="fw-bold text-decoration-none login-non-register-remember-password">ایجاد حساب</a>
+              </span>
+              <div>
+                <a className="fw-bold text-decoration-none login-non-register-remember-password">فراموشی رمز عبور</a>
+              </div>
+            </div>
+    
+            <hr className="my-4" />
+
+            <button type="button" className="btn login-google-btn d-flex align-items-center w-80 px-4 mx-auto">
+              <span className="fw-bold text-white ms-auto me-4 login-google-btn-enter-text">ورود با استفاده از</span>
+              <div className="d-flex align-items-center gap-2 me-auto">
+                <span className="fw-bold text-white login-google-btn-google-text">Google</span>
+                <img src="/src/assets/icons/google_icon.svg" alt="Google" width="23" />
+              </div>
+            </button>
+
+
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
+
+
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/css/bootstrap.rtl.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
 import './index.css'
-import ActiveUsersKPI from "./components/KPICharts/ActiveUsersKPI.tsx";
+// import ActiveUsersKPI from "./components/KPICharts/ActiveUsersKPI.tsx";
+import LoginPage from "./components/LoginPage/LoginPage";
+import './styles/fonts.css';
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ActiveUsersKPI />
+    <LoginPage />
   </StrictMode>,
 )
