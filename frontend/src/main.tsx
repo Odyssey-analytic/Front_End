9f950e66b00371511703688060d09971f66fa003
Niusha Yaghini <niusha.yaghini@gmail.com>
2025-04-13 17:15:14 +0330

    fix: add frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -17,7 +17,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         <Route path="/forgot-password" element={<ForgotPasswordPage />} /> 
         <Route path="/forgot-password/reset-password" element={<ResetpasswordPage />} />
