65c1ee027ff5668a42b9852da51e98110cfc70ac
Niusha Yaghini <niusha.yaghini@gmail.com>
2025-04-24 10:04:44 +0330

    fix: UI of welcome page

--- frontend/src/components/WelcomePage/WelcomePage.tsx
+++ frontend/src/components/WelcomePage/WelcomePage.tsx
@@ -58,37 +58,45 @@ const WelcomePage = () => {
   return (
     <>
       <div className="welcome-page-container vh-100 d-flex flex-column">
-        <div className="welcome-page-header d-flex justify-content-between align-items-center px-4 py-2 shadow-sm">
-          <div><img src={welcome_header_help} alt="Help" className="welcome-page-header-help-icon" /></div>
-          <div className="welcome-page-header-search-box flex-grow-1 mx-4">
-            <input type="text" className="welcome-page-header-search-box-input form-control text-center" placeholder="جستجو..." />
+
+        <div className="welcome-page-header">
+          <img src={welcome_header_help} alt="Help" className="welcome-page-header-help-icon" />
+
+          <div className="welcome-page-header-search-box">
+            <input
+              type="text"
+              className="welcome-page-header-search-box-input"
+              placeholder="جستجو..."
+            />
           </div>
-          <div className="d-flex align-items-center">
+
+          <div className="header-brand">
             <span className="website-brand-text english-text">ODESSAY</span>
             <img src={OdessayLogo} alt="Odessay Logo" className="website-logo-img ms-2" />
           </div>
         </div>
 
-        <div className="welcome-page-subheader d-flex justify-content-between align-items-center px-4 py-2">
+
+        <div className="welcome-page-subheader d-flex justify-content-between align-items-center py-2">
           <div className="d-flex align-items-center gap-3">
             <img src={welcome_subheader_menu} alt="Menu" className="welcome-page-subheader-menu-icon" />
             <img src={welcome_subheader_user} alt="User" className="welcome-page-subheader-user-icon" />
           </div>
-          <div className="welcome-page-subheader-admin-label-container d-flex align-items-center">
+          <div className="welcome-page-subheader-admin-label-container">
             <span className="welcome-page-subheader-admin-label-container-divider"></span>
             <div className="welcome-page-subheader-admin-label-container-label-text">Admin</div>
           </div>
         </div>
 
-        <div className="welcome-page-body d-flex justify-content-center align-items-center flex-grow-1">
-          <div className={`welcome-page-main-box text-center p-4 ${showPopup ? 'blurred' : ''}`}>
-            <div className="welcome-page-main-box-icon mb-3">
+        <div className="welcome-page-body justify-content-center align-items-center flex-grow-1">
+          <div className={`welcome-page-main-box text-center ${showPopup ? 'blurred' : ''}`}>
+            <div className="welcome-page-main-box-icon">
               <img src={welcome_page_main_box_welcome_icon} alt="Welcome Icon" />
             </div>
-            <h2 className="welcome-page-main-box-heading mb-3">{username} خوش اومدی!</h2>
-            <p className="welcome-page-main-box-description mb-1">شروع کن تا ببینی توی محصولات دقیقاً چه خبره</p>
-            <p className="welcome-page-main-box-description mb-4">و چطور می‌تونی بهترین تجربه رو برای کاربرات بسازی.</p>
-            <button className="btn welcome-page-main-box-start-btn" onClick={() => setShowPopup(true)}> اضافه کردن بازی </button>
+            <h2 className="welcome-page-main-box-heading">{username} خوش اومدی!</h2>
+            <p className="welcome-page-main-box-description">شروع کن تا ببینی توی محصولات دقیقاً چه خبره</p>
+            <p className="welcome-page-main-box-description">و چطور می‌تونی بهترین تجربه رو برای کاربرات بسازی.</p>
+            <button className="btn welcome-page-main-box-start-btn " onClick={() => setShowPopup(true)}> اضافه کردن بازی </button>
           </div>
         </div>
       </div>
@@ -131,71 +139,98 @@ const WelcomePage = () => {
                 </>
               )}
               {step === 2 && selectedProduct === 'game' && (
-  <div className="step-2-compact">
-    <p className="text-start mb-3">اطلاعات بازیت رو وارد کن :</p>
 
-    {/* نام بازی */}
-    <div className="text-start mb-4">
-      <label className="d-block mb-1">نام بازی :</label>
-      <input
-        type="text"
-        className="form-control game-name-input text-end"
-        value={gameName}
-        onChange={(e) => setGameName(e.target.value)}
-      />
-    </div>
+              <div className="step-2-compact">
+                <p className="text-start mb-3">اطلاعات بازیت رو وارد کن :</p>
 
-    <div className="d-flex flex-column flex-md-row gap-4 mb-4">
-      {/* موتور بازی */}
-      <div className="text-start flex-fill ">
-        <label className="d-block mb-1">انتخاب موتور بازی :</label>
-        <div className="d-flex gap-2 justify-content-start">
-          <label><input type="radio" checked={engine === 'Unity'} onChange={() => setEngine('Unity')} /> Unity</label>
-          <label className="engine-disabled-option"><input type="radio" disabled /> Godot</label>
-          <label className="engine-disabled-option"><input type="radio" disabled /> Custom</label>
-        </div>
-      </div>
+                {/* نام بازی */}
+                <div className="text-start mb-4">
+                  <label className="d-block mb-1">نام بازی:</label>
+                  <input
+                    type="text"
+                    className="form-control game-name-input text-end"
+                    value={gameName}
+                    onChange={(e) => setGameName(e.target.value)}
+                  />
+                </div>
 
-      <div className="text-start flex-fill mt-3 mt-md-0 mb-4">
-        <label className="d-block mb-1">انتخاب پلتفرم هدف :</label>
-        <div className="d-flex flex-row align-items-center gap-3">
-          <label><input type="checkbox" onChange={() => handlePlatformChange('Windows')} /> Windows</label>
-          <label><input type="checkbox" onChange={() => handlePlatformChange('Android')} /> Android</label>
-          <label><input type="checkbox" onChange={() => handlePlatformChange('iOS')} /> iOS</label>
-        </div>
-      </div>
-    </div>
+                {/* <!-- انتخاب موتور بازی --> */}
+                <div className="d-flex flex-column flex-md-row gap-4 mb-4">
+                  <div className="text-start flex-fill mt-3 mt-md-0 mb-4">
+                    <label className="d-block mb-3">انتخاب موتور بازی:</label>
 
-    <div className="w-100 text-start mb-4">
-      <label className="d-block mb-2">توضیحات (اختیاری) :</label>
-      <textarea
-        className="form-control"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-    </div>
+                    <div className="radio-group-grid">
+                      <label className="radio-label">
+                        <input type="radio" checked={engine === 'Unity'} onChange={() => setEngine('Unity')} />
+                        Unity
+                      </label>
 
-    <div className="w-100 text-start mb-5">
-      <label className="d-block mb-2">آپلود تصویر (اختیاری):</label>
-      <input
-        type="file"
-        accept="image/*"
-        className="form-control"
-        onChange={(e) => {
-          if (e.target.files && e.target.files[0]) {
-            setThumbnail(e.target.files[0]);
-          }
-        }}
-      />
-    </div>
+                      <label className="radio-label engine-disabled-option">
+                        <input type="radio" disabled />
+                        Godot
+                      </label>
 
-    {/* دکمه‌ها */}
-    <div className="d-flex justify-content-between mt-3">
-      <button className="btn btn-secondary" onClick={() => setStep(1)}>بازگشت</button>
-      <button className="btn btn-primary" onClick={handleSubmitGame}>ثبت</button>
-    </div>
-  </div>
-)}
+                      <label className="radio-label engine-disabled-option">
+                        <input type="radio" disabled />
+                        Custom
+                      </label>
+                    </div>
+                  </div>
+                </div>
+
+                {/* <!-- انتخاب پلتفرم هدف --> */}
+                <div className="d-flex flex-column flex-md-row gap-4 mb-4">
+                  <div className="text-start flex-fill mt-3 mt-md-0 mb-4">
+                    <label className="d-block mb-3">انتخاب پلتفرم هدف :</label>
+
+                    <div className="radio-group-grid">
+                      <label className="radio-label">
+                        <input type="radio" name="platform" onChange={() => handlePlatformChange('iOS')} />
+                        iOS
+                      </label>
+                      <label className="radio-label">
+                        <input type="radio" name="platform" onChange={() => handlePlatformChange('Android')} />
+                        Android
+                      </label>
+                      <label className="radio-label">
+                        <input type="radio" name="platform" onChange={() => handlePlatformChange('Windows')} />
+                        Windows
+                      </label>
+                      
+                    </div>
+                  </div>
+                </div>
+
+                <div className="w-100 text-start mb-4">
+                  <label className="d-block mb-2">توضیحات (اختیاری) :</label>
+                  <textarea
+                    className="form-control"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                  />
+                </div>
+
+                <div className="w-100 text-start mb-5">
+                  <label className="d-block mb-2">آپلود تصویر (اختیاری):</label>
+                  <input
+                    type="file"
+                    accept="image/*"
+                    className="form-control"
+                    onChange={(e) => {
+                      if (e.target.files && e.target.files[0]) {
+                        setThumbnail(e.target.files[0]);
+                      }
+                    }}
+                  />
+                </div>
+
+                {/* دکمه‌ها */}
+                <div className="d-flex justify-content-between mt-3">
+                  <button className="btn btn-secondary" onClick={() => setStep(1)}>بازگشت</button>
+                  <button className="btn btn-primary" onClick={handleSubmitGame}>ثبت</button>
+                </div>
+              </div>
+            )}
 
 
               {/* {step === 2 && selectedProduct === 'game' && (
