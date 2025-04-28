71c3204da00d27377fdfcc5a18e1115ce062fa01
Niusha Yaghini <niusha.yaghini@gmail.com>
2025-04-29 02:45:47 +0330

    fix: the overlapping off css codes

--- frontend/src/components/MainLayout/MainLayout.tsx
+++ frontend/src/components/MainLayout/MainLayout.tsx
@@ -8,7 +8,8 @@ import welcome_subheader_menu from '/public/icons/welcome_subheader_menu.svg';
 import welcome_header_help from '/public/icons/welcome_header_help.svg';
 
 
-const MainLayout = ({ children }: { children: React.ReactNode }) => {
+// const MainLayout = ({ children }: { children: React.ReactNode }) => {
+const MainLayout = () => {
   const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
 
@@ -21,41 +22,41 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="main-layout">
 
-        <div className="welcome-page-header">
-          <img src={welcome_header_help} alt="Help" className="welcome-page-header-help-icon" />
+        <div className="main-layout-header">
+          <img src={welcome_header_help} alt="Help" className="main-layout-header-help-icon" />
 
-          <div className="welcome-page-header-search-box">
+          <div className="main-layout-header-search-box">
             <input
               type="text"
-              className="welcome-page-header-search-box-input"
+              className="main-layout-header-search-box-input"
               placeholder="جستجو..."
             />
           </div>
 
-          <div className="header-brand">
-            <span className="website-brand-text english-text">ODESSAY</span>
-            <img src={OdessayLogo} alt="Odessay Logo" className="website-logo-img ms-2" />
+          <div className="main-layout-header-brand">
+            <span className="main-layout-brand-text english-text">ODESSAY</span>
+            <img src={OdessayLogo} alt="Odessay Logo" className="main-layout-logo-img ms-2" />
           </div>
         </div>
 
-        <div className="welcome-page-subheader d-flex justify-content-between align-items-center py-2">
+        <div className="main-layout-subheader d-flex justify-content-between align-items-center py-2">
             
 
           <div className="d-flex align-items-center gap-3">
 
             <div
-              className="welcome-page-menu-icon-wrapper"
+              className="main-layout-menu-icon-wrapper"
               onClick={() => setMenuOpen(prev => !prev)}
             >
               <img
                 src={welcome_subheader_menu}
                 alt="Menu"
-                className="welcome-page-subheader-menu-icon"
+                className="main-layout-subheader-menu-icon"
               />
 
               {menuOpen && (
-                <div className="welcome-page-dropdown-menu">
-                  <button className="welcome-page-dropdown-item" onClick={handleLogout}>
+                <div className="main-layout-dropdown-menu">
+                  <button className="main-layout-dropdown-item" onClick={handleLogout}>
                     خروج از حساب کاربری
                   </button>
                 </div>
@@ -63,21 +64,21 @@ const MainLayout = ({ children }: { children: React.ReactNode }) => {
 
             </div>
 
-            <img src={welcome_subheader_user} alt="User" className="welcome-page-subheader-user-icon" />
+            <img src={welcome_subheader_user} alt="User" className="main-layout-subheader-user-icon" />
           </div>
 
-          {/* <span className="welcome-page-subheader-admin-label-container-divider"></span>
-          <div className="welcome-page-subheader-admin-label-container-label-text">Admin</div> */}
+          {/* <span className="main-layout-subheader-admin-label-container-divider"></span>
+          <div className="main-layout-subheader-admin-label-container-label-text">Admin</div> */}
           
 
-          {/* <div className="welcome-page-subheader-admin-label-container">
-            <span className="welcome-page-subheader-admin-label-container-divider"></span>
-            <div className="welcome-page-subheader-admin-label-container-label-text">Admin</div>
+          {/* <div className="main-layout-subheader-admin-label-container">
+            <span className="main-layout-subheader-admin-label-container-divider"></span>
+            <div className="main-layout-subheader-admin-label-container-label-text">Admin</div>
           </div> */}
 
-          <div className="main-layout-content-children">
+          {/* <div className="main-layout-content-children">
             {children}
-          </div>
+          </div> */}
 
         </div>
 
